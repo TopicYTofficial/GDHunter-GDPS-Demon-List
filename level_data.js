987ecb07129341dbe0713ee8c2e3732411b6c074
TopicYTofficial <vladbad777xxxx@gmail.com>
2025-06-14 18:04:24 +0300

    level_data.js

---
+++ level_data.js
@@ -0,0 +1,103 @@
+const levelData = [ //место в топе зависит от положения в массиве
+    {
+        name: 'UnMaded',
+        author: 'royalGG',
+        id: 257
+    },
+
+    {
+        name: 'uaua factorial',
+        author: 'ExactZ',
+        id: 212
+    },
+
+    {
+        name: 'amethyst challenge',
+        author: 'ItzMiny',
+        id: 165
+    },
+
+    {
+        name: 'dep',
+        author: 'ItzMiny',
+        id: 327
+    },
+
+    {
+        name: 'untitled unnamed',
+        author: 'ExactZ',
+        id: 217
+    },
+
+    {
+        name: 'STEREO BUFFNESS',
+        author: 'Utochka',
+        id: 173
+    },
+
+    {
+        name: 'Boon',
+        author: 'ExactZ',
+        id: 117
+    },
+
+    {
+        name: 'The Egoist',
+        author: 'Setiop',
+        id: 211
+    },
+
+    {
+        name: 'green wave buff (UNRATED)',
+        author: 'ExactZ',
+        id: 181
+    },
+
+    {
+        name: 'Clutter God',
+        author: 'ExactZ',
+        id: 104
+    },
+
+    {
+        name: 'Green Wave',
+        author: 'ferx1n56',
+        id: 144
+    },
+
+    {
+        name: 'Top 1',
+        author: 'ferx1n56',
+        id: 147
+    },
+
+    {
+        name: 'HELL',
+        author: 'CsGoVERNITEDima',
+        id: 314
+    },
+
+    {
+        name: 'beZ doZi',
+        author: 'Beist',
+        id: 129
+    },
+
+    {
+        name: 'Creo Circles',
+        author: 'PELM3N',
+        id: 251
+    },
+
+    {
+        name: 'The Prodigy',
+        author: 'Setiop',
+        id: 199
+    },
+
+    {
+        name: 'Undecored Chaos',
+        author: 'Setiop',
+        id: 155
+    },
+];
